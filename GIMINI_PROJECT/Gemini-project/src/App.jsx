97c9ab816a02c470d57656d2c5733dd04d709201
Vksahu2211 <vksahu221119@gmail.com>
2025-01-17 00:52:04 +0530

    Add files via upload

---
+++ GIMINI_PROJECT/Gemini-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./component/Sidebar/Sidebar";
+import Main from "./component/Main/Main";
+
+const app = () =>{
+  return(
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+export default app;
